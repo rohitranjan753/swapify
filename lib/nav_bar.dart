c4ef3456f69a2d9a17eb566436b43f10bc8dd987
Rohit Ranjan <rohitgns54321@gmail.com>
2023-05-31 10:18:01 +0530

    chat

--- lib/nav_bar.dart
+++ lib/nav_bar.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:line_icons/line_icons.dart';
 import 'package:vbuddyproject/AddPageDir/add_page.dart';
-import 'package:vbuddyproject/Chat/chat_main_screen.dart';
 import 'package:vbuddyproject/HomePageDir/home_page.dart';
 import 'package:vbuddyproject/ProfilePageDir/ProfilePage.dart';
 import 'package:vbuddyproject/SearchPageDir/SearchPage.dart';
