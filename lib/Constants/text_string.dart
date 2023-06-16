e1dd5a728922f149564d7b33b478dfbfd75b2c04
Rohit Ranjan <rohitgns54321@gmail.com>
2023-06-16 09:06:51 +0530

    text constant

--- lib/Constants/text_string.dart
+++ lib/Constants/text_string.dart
@@ -1,7 +1,22 @@
+//Add page String
 const titleHint = "Enter Title";
 const descriptionHint = "Enter Description";
 const sellPriceHint = "Enter Price";
 const rentPriceHint = "Enter Price / 6Hrs";
+const itemCategoryHint = "Enter Item Category";
+const itemSubCategoryHint = "Enter Sub Category";
 
 
 
+// selected Page string
+const categoryText = "Category";
+const descriptionText = "Description";
+const uploadedbyText = "Uploaded By";
+const uploadedonText = "Uploaded On";
+const priceText = "Price";
+
+
+
+//Button Text
+const submitBtn = "SUBMIT";
+const chatBtn = "CHAT";
