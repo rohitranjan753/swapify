0a7b9702e50ea8e46b181bf5f5226dc5fe8caf98
Rohit Ranjan <rohitgns54321@gmail.com>
2023-06-16 10:47:08 +0530

    splash

--- lib/SplashScreen/splash_screen.dart
+++ lib/SplashScreen/splash_screen.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:vbuddyproject/Constants/sizes.dart';
 
 class SplashScreen extends StatelessWidget {
-  
-
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            left: 0,
+            child: Image(
+              image: AssetImage("assets/splash/trade.png"),
+            ),
+          ),
+          Positioned(
+            top: 80,
+            left: tDefaultSize,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Welcome to vBuddy",
+                        style: Theme.of(context).textTheme.headline4,
+                      ),
+                      Text(
+                        "Sell your belongings",
+                        style: Theme.of(context).textTheme.bodyText1,
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+            )
+          ),
+
+        ],
+      ),
+    );
   }
 }
