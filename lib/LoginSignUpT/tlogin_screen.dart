a0048b097eae255bef2790fae85b86325784db35
Rohit Ranjan <rohitgns54321@gmail.com>
2023-06-17 08:45:47 +0530

    New login page

--- lib/LoginSignUpT/tlogin_screen.dart
+++ lib/LoginSignUpT/tlogin_screen.dart
@@ -15,48 +15,96 @@ class TLoginScreen extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Image(
-                  image: AssetImage("assets/splash/splash.png"),
-                  height: size.height * 0.2,
-                ),
-                Text(
-                  "Welcome Back",
-                  style: Theme.of(context).textTheme.headline4,
-                ),
-                Text(
-                  "Make it, take it, Risk it",
-                  style: Theme.of(context).textTheme.bodyText1,
-                ),
-                Form(
-                  child: Container(
-                    padding: EdgeInsets.symmetric(vertical: 20.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        TextFormField(
-                          decoration: InputDecoration(
-                            prefixIcon: Icon(Icons.person_outline_outlined),
-                            labelText: "Email",
-                            hintText: "Email Hint",
-                            border: OutlineInputBorder()
-                          ),
-                        ),
-
-                        TextFormField(
-                          decoration: InputDecoration(
-                              prefixIcon: Icon(Icons.person_outline_outlined),
-                              labelText: ,
-                              hintText: "Email Hint",
-                              border: OutlineInputBorder()
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
+                LoginHeaderWidget(size: size),
+                const LoginForm(),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text("OR"),
+                    OutlinedButton.icon(onPressed: (){}, icon: Image(image: AssetImage("assets/googlelogo.png"),), label: Text("Sign-In with Google"),)
+                  ],
+                )
               ],
             ),
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class LoginHeaderWidget extends StatelessWidget {
+  const LoginHeaderWidget({
+    Key? key,
+    required this.size,
+  }) : super(key: key);
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Image(
+          image: AssetImage("assets/splash/splash.png"),
+          height: size.height * 0.2,
+        ),
+        Text(
+          "Welcome Back",
+          style: Theme.of(context).textTheme.headline4,
+        ),
+        Text(
+          "Make it, take it, Risk it",
+          style: Theme.of(context).textTheme.bodyText1,
+        ),
+      ],
+    );
+  }
+}
+
+class LoginForm extends StatelessWidget {
+  const LoginForm({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.person_outline_outlined),
+                  labelText: "Email",
+                  hintText: "Email Hint",
+                  border: OutlineInputBorder()),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.fingerprint_outlined),
+                  labelText: "Password",
+                  hintText: "Password Hint",
+                  border: OutlineInputBorder(),
+                  suffixIcon: IconButton(
+                    onPressed: null,
+                    icon: Icon(Icons.remove_red_eye_sharp),
+                  )),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("LOGIN"),
+            )
+          ],
         ),
       ),
     );
