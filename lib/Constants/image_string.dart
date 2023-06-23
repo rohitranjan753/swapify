aa2c195ca1137b81f85a6fe9a1e93e321463781e
Rohit Ranjan <rohitgns54321@gmail.com>
2023-06-23 21:23:26 +0530

    lottie

---
+++ lib/Constants/image_string.dart
@@ -0,0 +1,5 @@
+//Login and Signup
+const loginImageLottie = 'assets/welcome_hi.json';
+const loginImageStatic = 'assets/login_welcome.png';
+const signupImageLottie = 'assets/signup_lottie.json';
+const signupImageStatic = 'assets/signup_static.png';
