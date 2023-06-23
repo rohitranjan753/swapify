aa7835edc7f2d928322fc17d3c2581e178a15619
Rohit Ranjan <rohitgns54321@gmail.com>
2023-06-23 21:26:24 +0530

    lottie

--- lib/Constants/image_string.dart
+++ lib/Constants/image_string.dart
@@ -3,3 +3,14 @@ const loginImageLottie = 'assets/welcome_hi.json';
 const loginImageStatic = 'assets/login_welcome.png';
 const signupImageLottie = 'assets/signup_lottie.json';
 const signupImageStatic = 'assets/signup_static.png';
+
+//Home Page
+const homepageBuy = 'assets/images/buy_new.jpg';
+const homepageRent = 'assets/images/rent_new.jpg';
+
+
+//Add Page
+const addpageSell = 'assets/images/buy_new.jpg';
+const addpageRent = 'assets/images/buy_new.jpg';
+
+
